ea9221bfa28db9ec6ed4eece368affbb5c949bf0
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2023-11-14 22:52:14 +1100

    Templates and Views
    Sample view and welcome.html in templates folder

--- sports_club/members/urls.py
+++ sports_club/members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("members/", views.members, name = "members")
+    path("members/", views.members, name = "members"),
+    path("sample/", views.sample, name = "sample")
 ]
--- sports_club/members/views.py
+++ sports_club/members/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 # Members view 
 def members(request):
-    message = "I sang the whole Hanuman Chalisa in Hindi"
+    template = loader.get_template("welcome.html")
+    return HttpResponse(template.render())
+
+# Sample view
+def sample(request):
+    message = "This content is coming from sample view of members application"
     return HttpResponse(message)
-    
