853636868692e545125167776be955e96c21bb64
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2023-11-20 23:53:39 +1100

    Model
    Created Member Model and add Records to the model

--- sports_club/members/models.py
+++ sports_club/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# Member model
+class Member(models.Model):
+    fName = models.CharField(max_length=255)
+    lName = models.CharField(max_length=255)
