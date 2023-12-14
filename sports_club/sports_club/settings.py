8c8e49bc61e334175cf29f398fdd1bb2ae17b55e
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2023-12-14 23:21:01 +1100

    static
    Created static folder for styling.

--- sports_club/sports_club/settings.py
+++ sports_club/sports_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6yt+4!+^2$rwet6$6_+3*@%qzc$8d)!p9lw^@sz%z2!)p)kw6u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
