88e7c9a4da2595e09690d48add869aceafe72261
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-01-12 23:30:32 +1100

    Global and Style
    Applying global style and styling of the project

--- sports_club/sports_club/settings.py
+++ sports_club/sports_club/settings.py
@@ -121,6 +121,10 @@ STATIC_ROOT = BASE_DIR / 'prodFiles'
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'global'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
