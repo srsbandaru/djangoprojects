1c65a537670a6f5164c2063e13f606293a8b8cbb
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-01-13 23:37:47 +1100

    Set list_display property
    Configured the list_display property for member model

--- sports_club/members/admin.py
+++ sports_club/members/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Member
 
 # Register your models here.
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("fName","lName","joinedDate")
+
+
+admin.site.register(Member, MemberAdmin)
